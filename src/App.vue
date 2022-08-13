daf17090654cba3c28bf5af111ccea40b7bd065b
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2022-08-13 00:51:59 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,219 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-8">
+          <h4 class="mb-3">Room</h4>
+
+          <draggable
+            class="list-group room"
+            v-model="list1"
+            group="my-group"
+            tag="transition-group"
+            @start="drag = true"
+            @end="drag = false"
+            v-bind="dragOptions"
+            :component-data="{
+              tag: 'ul',
+              type: 'transition-group',
+              name: !drag ? 'flip-list' : null,
+            }"
+          >
+            <template #item="{ element }">
+              <li class="card shadow p-4">{{ element.name }}</li>
+            </template>
+          </draggable>
+        </div>
+
+        <div class="col">
+          <div class="row">
+            <h4 class="mb-3">Parks</h4>
+
+            <draggable
+              class="list-group park"
+              v-model="list2"
+              group="my-group"
+              tag="transition-group"
+              @start="drag = true"
+              @end="drag = false"
+              v-bind="dragOptions"
+              :component-data="{
+                tag: 'ul',
+                type: 'transition-group',
+                name: !drag ? 'flip-list' : null,
+              }"
+            >
+              <template #item="{ element }">
+                <li class="card shadow p-4">{{ element.name }}</li>
+              </template>
+            </draggable>
+          </div>
+
+          <div class="row mt-3">
+            <draggable
+              class="list-group park"
+              v-model="list3"
+              group="my-group"
+              tag="transition-group"
+              @start="drag = true"
+              @end="drag = false"
+              v-bind="dragOptions"
+              :component-data="{
+                tag: 'ul',
+                type: 'transition-group',
+                name: !drag ? 'flip-list' : null,
+              }"
+            >
+              <template #item="{ element }">
+                <li class="card shadow p-4">{{ element.name }}</li>
+              </template>
+            </draggable>
+          </div>
+
+          <div class="row mt-3">
+            <draggable
+              class="list-group park"
+              v-model="list4"
+              group="my-group"
+              tag="transition-group"
+              @start="drag = true"
+              @end="drag = false"
+              v-bind="dragOptions"
+              :component-data="{
+                tag: 'ul',
+                type: 'transition-group',
+                name: !drag ? 'flip-list' : null,
+              }"
+            >
+              <template #item="{ element }">
+                <li class="card shadow p-4">{{ element.name }}</li>
+              </template>
+            </draggable>
+          </div>
+
+          <div class="row mt-3">
+            <draggable
+              class="list-group park"
+              v-model="list5"
+              group="my-group"
+              tag="transition-group"
+              @start="drag = true"
+              @end="drag = false"
+              v-bind="dragOptions"
+              :move="limitList"
+              :component-data="{
+                tag: 'ul',
+                type: 'transition-group',
+                name: !drag ? 'flip-list' : null,
+              }"
+            >
+              <template #item="{ element }">
+                <li class="card shadow p-4">{{ element.name }}</li>
+              </template>
+            </draggable>
+          </div>
+          <div class="row mt-3">
+            <draggable
+              class="list-group park"
+              v-model="list6"
+              group="my-group"
+              tag="transition-group"
+              @start="drag = true"
+              @end="drag = false"
+              v-bind="dragOptions"
+              :component-data="{
+                tag: 'ul',
+                type: 'transition-group',
+                name: !drag ? 'flip-list' : null,
+              }"
+            >
+            <!-- :move="() => { if (list6.length >= 1) return false }" -->
+              <template #item="{ element }">
+                <li class="card shadow p-4">{{ element.name }}</li>
+              </template>
+            </draggable>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import draggable from "vuedraggable";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    draggable,
+  },
+  data() {
+    return {
+      list1: [
+        { id: 1, name: "Agente 1001" },
+        { id: 2, name: "Agente 1002" },
+        { id: 3, name: "Agente 1003" },
+        { id: 4, name: "Agente 1004" },
+        { id: 5, name: "Agente 1005" },
+        { id: 6, name: "Agente 1006" },
+      ],
+      list2: [],
+      list3: [],
+      list4: [],
+      list5: [],
+      list6: [],
+    };
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost",
+      };
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+ul {
+  list-style: none !important;
 }
+
+li {
+  cursor: move !important;
+  height: fit-content;
+}
+
+ul.room {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 200px);
+  grid-auto-rows: max-content;
+  gap: 1rem;
+  height: 100%;
+}
+
+ul.park {
+  display: grid;
+  gap: 1rem;
+  height: 106px;
+  padding: 1rem;
+  background-color: #e6e6e6;
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #e6e6e6;
+}
+
+.list-group {
+  min-height: 20px;
+}
+
 </style>
---
+++ src/components/DraggableContainer.vue
@@ -0,0 +1,39 @@
+<template>
+  <draggable
+  v-model="list"
+  tag="transition-group"
+  item-key="order">
+    <template #item="{ element }">
+      <div>{{ element.name }}</div>
+    </template>
+  </draggable>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+const message = [
+  "vue.draggable",
+  "draggable",
+  "component",
+  "for",
+  "vue.js 2.0",
+  "based",
+  "on",
+  "Sortablejs"
+];
+
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+    list: message.map((name, index) => {
+        return { name, order: index + 1 };
+    }),
+    drag: false,
+    };
+  },
+};
+</script>
